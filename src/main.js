a857eee720d739dbca350a0ac8b5b5f16da50105
w1zlled2 <filin.vitalik2000@mail.ru>
2024-04-05 00:24:31 +0500

    Настройка router/index.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+router.beforeEach((to, from, next) => {
+   document.title = to.meta.title;
+   next();
+})
+
 createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,14 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {title: "Главная"}
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    meta: {title: "О нас"},
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
